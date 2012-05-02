55d6f130650946214b685677f0d62c885da76786
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-05-02 12:23:37 -0400

    Added variables, Initial output and the emmaCare conditional. Started the initial output for mookie.

--- sdi-project1/johnson_bernice_assignment1.js
+++ sdi-project1/johnson_bernice_assignment1.js
@@ -5,5 +5,32 @@
 
 alert("Planning my trip to Washington D.C.");
 
+//Initial variables
+var           place = "Washington D.C.";
+var        daughter = "Emma";
+var             dog = "Mookie";
+var           money = 200 ;
+var           train = true ;
+var    readyToLeave = true ;
+var        emmaCare = true;
+var      mookieCare = false;
 
-console.log("hello world");
+
+//Initial output
+console.log ("We are planning for our trip to " + place);
+console.log ("First we need to make sure that my little girl " + daughter + " is taken care of!");
+
+
+
+//emmaCare conditional. Did we find a babysitter for Emma?
+  if (emmaCare===true){
+  	 console.log("We have decided that she will be staying at her Dad's house. Awesome!"); 
+
+  	}else{
+  	  console.log("Ask other family members.");
+
+
+  };
+
+  //Initial output
+  console.log("Next we need to figure out if our little pooch " + dog + " will be going with us.");
